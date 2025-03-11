8ad07e8a47c510240edd758be7fef1366ebb6b6d
ick-web <yih000098@gmail.com>
2025-03-11 09:34:10 +0900

    save

--- part2/04.클로저/(lv.3)설정값-보존하기.js
+++ part2/04.클로저/(lv.3)설정값-보존하기.js
@@ -15,7 +15,17 @@
  */
 
 // TODO: createConfigManager 함수를 작성하세요.
-function createConfigManager() {}
+function createConfigManager() {
+  let config = {};
+  const setConfig = (key, value) => {
+    config = { ...config, [key]: value };
+    return config;
+  };
+  const getConfig = (key) => {
+    return config[key];
+  };
+  return { setConfig, getConfig };
+}
 
 // export 를 수정하지 마세요.
 export { createConfigManager };
